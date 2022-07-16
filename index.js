f50d1077494f6d5f0dbd9c1d5b2131b737c8097b
JVSREco19 <joao.victor.silveira.r@unifei.edu.br>
2022-07-16 07:54:39 -0300

    CrudPadrão

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const config = require("./config");
+const bodyparser = require("body-parser");
+const routes = require("./src/routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyparser.json());
+app.use(routes);
+
+
+app.listen(config.port, () => {
+  console.log("Servidor funcionando na porta: " + config.port);
+});
